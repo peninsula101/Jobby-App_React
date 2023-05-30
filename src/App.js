62cd2c94ba2ca9451a76ab1bd8a5dde28a9cc70d
peninsula101 <tvscskiriti@gmail.com>
2023-05-30 06:49:55 +0000

    jobby app

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import {Route, Switch, Redirect} from 'react-router-dom'
+
+import Login from './components/Login'
+import Home from './components/Home'
+import JobItemDetails from './components/JobItemDetails'
+import Jobs from './components/Jobs'
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+
+import './App.css'
+
+// Replace your code here
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={Login} />
+    <ProtectedRoute exact path="/" component={Home} />
+    <ProtectedRoute exact path="/jobs" component={Jobs} />
+    <ProtectedRoute exact path="/jobs/:id" component={JobItemDetails} />
+    <Route path="/not-found" component={NotFound} />
+    <Redirect to="not-found" />
+  </Switch>
+)
+
+export default App
